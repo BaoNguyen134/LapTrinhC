7812d9b4cd4760dad10dd30275ecd04b123d0206
Nguyễn Tuấn Bảo <nguyentuanbao93@gmail.com>
2016-06-15 16:53:52 +0700

    Các kiểu dữ liệu

---
+++ Bai002_KieuDuLieu.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+int main(){
+	printf("Bo nho cua int: %d byte\n",sizeof(int));
+	printf("Bo nho cua int: %d byte\n",sizeof(char));
+	printf("Bo nho cua int: %d byte",sizeof(float));
+}
